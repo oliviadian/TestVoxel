58e527b547b6c06eaf510a04060870c6fbb0f9fe
oliviadian <oliviadiaan12@gmail.com>
2020-10-14 15:17:06 +0700

    result

---
+++ js/essay1.js
@@ -0,0 +1,2 @@
+var kata1 = "Jika Input yang dimasukkan";
+console.log(kata1.replace(/a/g, "i"))
---
+++ js/essay2.js
@@ -0,0 +1,24 @@
+var a1 = 8;
+var a2 = 7;
+var a3 = 6;
+var a4 = 9;
+var b1 = 5;
+var b2 = 8;
+var b3 = 9;
+var b4 = 6;
+
+int hasil = 0;
+
+
+if (a1 > b1) {
+    hasil += hasil;
+} if else(a2>b2){
+    hasil += hasil;
+} if else (a3>b3){
+    hasil =+ hasil;
+}else (a4>b4){
+    hasil =+ hasil;
+}
+
+
+console.log('hasil')
